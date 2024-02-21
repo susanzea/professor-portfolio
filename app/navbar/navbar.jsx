fdc932bc6f602f35578ff0a32568e9177b370769
Susan Zea <susanzea16@gmail.com>
2024-02-21 16:20:57 -0500

    desktop navbar styled

--- app/navbar/navbar.jsx
+++ app/navbar/navbar.jsx
@@ -1,10 +1,14 @@
+'use client';
 import { Source_Sans_3 } from 'next/font/google';
+import { useState } from 'react';
 import Link from 'next/link';
 import classes from './navbar.module.scss';
 
 const sourceSans3 = Source_Sans_3({ subsets: ['latin'], weight: '500' });
 
 const Navbar = () => {
+  const [selected, setSelected] = useState('Home');
+
   const navLinks = [
     { title: 'Home', href: '/home' },
     {
@@ -20,8 +24,16 @@ const Navbar = () => {
       <div className={classes.professorName}>Aníbal González</div>
       <nav className={`${sourceSans3.className} ${classes.nav}`}>
         {navLinks.map((nl, i) => (
-          <Link key={i} className={classes[nl.className]} href={nl.href}>
+          <Link
+            key={i}
+            onClick={() => setSelected(nl.title)}
+            className={`${
+              nl.title === selected ? classes.selected : classes.notSelected
+            } ${classes[nl.className]}`}
+            href={nl.href}
+          >
             {nl.title}
+            {/* <span className={`${classes[nl.className]}`}>{nl.title}</span> */}
           </Link>
         ))}
       </nav>
