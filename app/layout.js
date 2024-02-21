4f81c6d1564372a22e215929de1e285eba890a77
Susan Zea <susanzea16@gmail.com>
2024-02-21 15:22:16 -0500

    navbar added. professor name and nav links styled. working on margin between nav links and styling bottom border on nav links

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${openSans.className}`}>
+      <body className={openSans.className}>
         <Navbar />
         {children}
       </body>
--- app/navbar/navbar.jsx
+++ app/navbar/navbar.jsx
@@ -1,14 +1,29 @@
+import { Source_Sans_3 } from 'next/font/google';
 import Link from 'next/link';
+import classes from './navbar.module.scss';
+
+const sourceSans3 = Source_Sans_3({ subsets: ['latin'], weight: '500' });
 
 const Navbar = () => {
+  const navLinks = [
+    { title: 'Home', href: '/home' },
+    {
+      title: 'Academic Writing',
+      href: '/academic-writing',
+      className: 'academicWriting',
+    },
+    { title: 'Books', href: '/books' },
+    { title: 'Fiction', href: '/fiction' },
+  ];
   return (
-    <div id="navbar">
-      <div>Aníbal González</div>
-      <nav>
-        <Link href="/home">Home</Link>
-        <Link href="/academic-writing">Academic Writing</Link>
-        <Link href="/books">Books</Link>
-        <Link href="/fiction">Fiction</Link>
+    <div className={classes.navbar}>
+      <div className={classes.professorName}>Aníbal González</div>
+      <nav className={`${sourceSans3.className} ${classes.nav}`}>
+        {navLinks.map((nl, i) => (
+          <Link key={i} className={classes[nl.className]} href={nl.href}>
+            {nl.title}
+          </Link>
+        ))}
       </nav>
     </div>
   );
