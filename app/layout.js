10e6564911e6c411f916c168d0aa9869e9925605
Susan Zea <susanzea16@gmail.com>
2024-02-20 21:49:25 -0500

    removed test class

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`hi ${openSans.className}`}>{children}</body>
+      <body className={`${openSans.className}`}>{children}</body>
     </html>
   );
 }
