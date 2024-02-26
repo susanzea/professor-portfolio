debea0437cbd6f9e35b3ee088f121ea75f526da2
Susan Zea <susanzea16@gmail.com>
2024-02-25 23:33:16 -0500

    restyled mobile nav menu sizing

--- app/navbar/navbarOne.jsx
+++ app/navbar/navbarOne.jsx
@@ -38,89 +38,6 @@ const navLinks = [
   },
 ];
 
-const hideNavItemsVariant = {
-  opened: {
-    opacity: 0,
-    y: '-100%',
-    transition: {
-      duration: 0.5,
-      ease: 'easeInOut',
-    },
-  },
-  closed: {
-    opacity: 1,
-    y: '0%',
-    transition: {
-      delay: 1.1,
-      duration: 0.5,
-      ease: 'easeInOut',
-    },
-  },
-};
-
-const mobileMenuVariant = {
-  opened: {
-    y: '0%',
-    transition: {
-      delay: 0.15,
-      duration: 1.1,
-      ease: [0.74, 0, 0.19, 1.02],
-    },
-  },
-  closed: {
-    y: '-100%',
-    transition: {
-      delay: 0.35,
-      duration: 0.63,
-      ease: [0.74, 0, 0.19, 1.02],
-    },
-  },
-};
-
-const fadeInVariant = {
-  opened: {
-    opacity: 1,
-    transition: {
-      delay: 1.2,
-    },
-  },
-  closed: { opacity: 0 },
-};
-
-const ulVariant = {
-  opened: {
-    transition: {
-      delayChildren: 1,
-      staggerChildren: 0.18,
-    },
-  },
-  closed: {
-    transition: {
-      staggerChildren: 0.06,
-      staggerDirection: -1,
-    },
-  },
-};
-
-const liVariant = {
-  opened: {
-    opacity: 1,
-    y: '0%',
-    transition: {
-      duration: 0.65,
-      ease: 'easeOut',
-    },
-  },
-  closed: {
-    opacity: 0,
-    y: '100%',
-    transition: {
-      duration: 0.25,
-      ease: 'easeInOut',
-    },
-  },
-};
-
 const ResponsiveNavbar = ({
   selected,
   setSelected,
@@ -132,27 +49,44 @@ const ResponsiveNavbar = ({
   };
 
   return (
-    // <div className={`${classes.overlay} overlay`}>
-    <motion.div variants={mobileMenuVariant} className={classes.mobileMenu}>
-      <motion.button
-        variants={fadeInVariant}
-        onClick={() => setIsResponsiveNavbarOpen(false)}
-      >
-        Close
-      </motion.button>
-      <motion.ul variants={ulVariant}>
-        {navLinks.map((navItem) => (
-          <motion.li whileTap={{ scale: 0.95 }} key={navItem.id}>
-            <motion.div variants={liVariant}>{navItem.title}</motion.div>
-          </motion.li>
-        ))}
-      </motion.ul>
-      <motion.div variants={fadeInVariant} className="contact">
-        <h5>+852 5650 2233</h5>
-        <h5>hi@designagency.com</h5>
-      </motion.div>
-    </motion.div>
-    // </div>
+    <div className={`${classes.overlay} overlay`}>
+      <ClickAway onClickAway={() => setIsResponsiveNavbarOpen(false)}>
+        <div
+          open={true}
+          // setOpen={handleOnNavItemClick}
+          className={`${classes.responsiveNavbar} uppercase`}
+        >
+          <IconButton
+            className={classes.close}
+            onClick={() => setIsResponsiveNavbarOpen(false)}
+          />
+          {/* <div className={`${classes.professorName}`}>Aníbal González</div> */}
+          <nav>
+            {navLinks.map((nl, i) => (
+              <div className={classes.navItem} key={i}>
+                <Image
+                  src={nl.icon.src}
+                  alt={nl.icon.alt}
+                  width={20}
+                  height={20}
+                />
+                <Link
+                  onClick={() => handleOnNavItemClick(nl.title)}
+                  className={`${
+                    nl.title === selected
+                      ? classes.selected
+                      : classes.notSelected
+                  } ${sourceSans3.className}`}
+                  href={nl.href}
+                >
+                  {nl.title}
+                </Link>
+              </div>
+            ))}
+          </nav>
+        </div>
+      </ClickAway>
+    </div>
   );
 };
 
