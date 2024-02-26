1d6f94117a9d148e3ab9b2b376b26b99e9416dc8
Susan Zea <susanzea16@gmail.com>
2024-02-26 01:39:39 -0500

    updated page class, added headshot

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 import classes from './home.module.scss';
 
 const Home = () => {
   return (
     <div className={`${classes.home} page`}>
-      <h3>Homeee</h3>
+      <Image
+        src={'/headshot.jpeg'}
+        alt={'photo of Aníbal González'}
+        height={300}
+        width={300}
+        style={{ objectFit: 'cover', maxWidth: '60%' }}
+      />
     </div>
   );
 };
