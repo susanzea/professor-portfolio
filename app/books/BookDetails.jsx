d1b731f6faa30cc2bd036464e48832772dd72cfa
Susan Zea <susanzea16@gmail.com>
2024-03-21 17:22:24 -0400

    added book data and wired BookDetails

--- app/books/BookDetails.jsx
+++ app/books/BookDetails.jsx
@@ -1,5 +1,22 @@
-const BookDetails = () => {
-  return <div id="book-details">Book details</div>;
+import Image from 'next/image';
+
+const BookDetails = ({ selectedBook }) => {
+  return (
+    <div id="book-details">
+      <Image
+        className="cover"
+        src={selectedBook.image}
+        alt={'photo of book cover'}
+        height={500}
+        width={350}
+        style={{ objectFit: 'cover' }}
+      />
+      <div className="text">
+        <h4>{selectedBook.title}</h4>
+        <p>{selectedBook.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default BookDetails;
--- app/books/page.jsx
+++ app/books/page.jsx
@@ -6,15 +6,30 @@ import './books.scss';
 
 const booksByIdx = {
   0: {
-    title: 'book0',
+    title:
+      'In Search of the Sacred Book: Religion and the Contemporary Latin American Novel',
+    image: '/sacred-book.jpg',
+    description: 'this is a book description 0',
+  },
+  0: {
+    title: 'Redentores',
+    image: '/redentores.jpeg',
+    description: 'this is a book description 0',
+  },
+  0: {
+    title: 'A Companion to Spanish American Modernismo',
+    image: '/companion-modernismo.jpg',
     description: 'this is a book description 0',
   },
   1: {
-    title: 'book1',
+    title: 'Love and Politics in the Contemporary Spanish American Novel',
+    image: '/love-and-politics.jpg',
     description: 'this is a book description 1',
   },
   2: {
-    title: 'book2',
+    title:
+      'Killer Books: Writing, Violence, and Ethics in Modern Spanish American Narrative',
+    image: '/killer-books.jpg',
     description: 'this is a book description 2',
   },
 };
