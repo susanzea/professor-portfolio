bf5632d4f2a6698bbd2dfeb24ca7237f52d87dfa
Susan Zea <susanzea16@gmail.com>
2024-03-23 16:01:34 -0400

    books index on click function and base sizing and flexing styled

--- app/books/BooksIndex.jsx
+++ app/books/BooksIndex.jsx
@@ -1,5 +1,42 @@
-const BooksIndex = () => {
-  return <div id="books-index">Books index</div>;
+import Image from 'next/image';
+
+const BookItem = ({ idx, bookData, setSelectedBookIdx }) => {
+  return (
+    <div
+      className="book-item"
+      role="button"
+      onClick={() => setSelectedBookIdx(idx)}
+    >
+      <Image
+        className="cover"
+        src={bookData.image}
+        alt={`book cover of ${bookData.title}`}
+        height={150}
+        width={100}
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  );
+};
+
+const BooksIndex = ({ booksByIdx, selectedBookIdx, setSelectedBookIdx }) => {
+  const keys = Object.keys(booksByIdx);
+
+  return (
+    <div id="books-index">
+      {keys.map((idx) => {
+        if (idx !== selectedBookIdx)
+          return (
+            <BookItem
+              key={idx}
+              idx={idx}
+              bookData={booksByIdx[idx]}
+              setSelectedBookIdx={setSelectedBookIdx}
+            />
+          );
+      })}
+    </div>
+  );
 };
 
 export default BooksIndex;
--- app/books/page.jsx
+++ app/books/page.jsx
@@ -6,13 +6,17 @@ import { booksByIdx } from './bookData';
 import './books.scss';
 
 const Books = () => {
-  const [selectedBookIdx, setSelectedBookIdx] = useState(0);
+  const [selectedBookIdx, setSelectedBookIdx] = useState('0');
 
   return (
     <div className={'books page'}>
       <div className="content-container">
         <BookDetails selectedBook={booksByIdx[selectedBookIdx]} />
-        <BooksIndex setSelectedBookIdx={setSelectedBookIdx} />
+        <BooksIndex
+          booksByIdx={booksByIdx}
+          selectedBookIdx={selectedBookIdx}
+          setSelectedBookIdx={setSelectedBookIdx}
+        />
       </div>
     </div>
   );
