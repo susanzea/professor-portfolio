6d29b162ab58dcceb9a53d067fbae229d48a1a03
Susan Zea <susanzea16@gmail.com>
2024-03-23 17:13:56 -0400

    book descriptions fully updated for Sacred and Redentores

--- app/books/bookData.js
+++ app/books/bookData.js
@@ -3,13 +3,74 @@ export const booksByIdx = {
     title:
       'In Search of the Sacred Book: Religion and the Contemporary Latin American Novel',
     image: '/sacred-book.jpg',
-    description:
-      'In Search of the Sacred Book studies the artistic incorporation of religious concepts such as prophecy, eternity, and the afterlife in the contemporary Latin American novel. It departs from sociopolitical readings by noting the continued relevance of religion in Latin American life and culture, despite modernity’s powerful secularizing influence. Analyzing Jorge Luis Borges’s secularized “narrative theology” in his essays and short stories, the book follows the development of the Latin American novel from the early twentieth century until today by examining the attempts of major novelists, from María Luisa Bombal, Alejo Carpentier, and Juan Rulfo, to Julio Cortázar, Gabriel García Márquez, and José Lezama Lima, to “sacralize” the novel by incorporating traits present in the sacred texts of many religions. It concludes with a view of the “desacralization” of the novel by more recent authors, from Elena Poniatowska and Fernando Vallejo to Roberto Bolaño.',
+    description: (
+      <p>
+        In Search of the Sacred Book studies the artistic incorporation of
+        religious concepts such as prophecy, eternity, and the afterlife in the
+        contemporary Latin American novel. It departs from sociopolitical
+        readings by noting the continued relevance of religion in Latin American
+        life and culture, despite modernity’s powerful secularizing influence.
+        Analyzing Jorge Luis Borges’s secularized “narrative theology” in his
+        essays and short stories, the book follows the development of the Latin
+        American novel from the early twentieth century until today by examining
+        the attempts of major novelists, from María Luisa Bombal, Alejo
+        Carpentier, and Juan Rulfo, to Julio Cortázar, Gabriel García Márquez,
+        and José Lezama Lima, to “sacralize” the novel by incorporating traits
+        present in the sacred texts of many religions. It concludes with a view
+        of the “desacralization” of the novel by more recent authors, from Elena
+        Poniatowska and Fernando Vallejo to Roberto Bolaño.
+        <br />
+        <br />
+        <a
+          className="link"
+          href="https://upittpress.org/books/9780822965046/"
+        >
+          Read more
+        </a>
+        <br />
+        <br />
+        <span className="bold"> Publication Date:</span>
+        <br />
+        <span>&nbsp;2010</span>
+      </p>
+    ),
   },
   1: {
     title: 'Redentores',
     image: '/redentores.jpeg',
-    description: 'this is a book description 0',
+    description: (
+      <p>
+        <span>Redentores by Manuel Zeno Gandía</span>
+        <br />
+        <br />
+        <span>Critical edition by Aníbal González-Pérez</span>
+        <br />
+        <br /> Manuel Zeno Gandía nació en Arecibo en 1855, hijo de hacendados
+        dedicados al cultivo de la caña de azúcar. Comenzó su carrera literaria
+        y de gestión cultural mientras cursaba estudios de medicina en Madrid.
+        Tuvo la oportunidad de coincidir, en sus estadías en España y en
+        Francia, con los momentos de mayor debate público sobre el Realismo y el
+        Naturalismo, las dos escuelas narrativas más importantes del siglo XIX
+        en Europa. En Puerto Rico, tuvo una vida activa como periodista —fue
+        propietario de La Correspondencia— y como político, habiendo militado en
+        grupos o partidos independentistas. Como escritor literario, cultivó con
+        éxito la novela, el cuento, la poesía y el ensayo crítico. Su novela La
+        charca (1894) ha tenido un éxito sostenido, siendo considerada la novela
+        cumbre del Naturalismo en Puerto Rico. Esta es la primera edición
+        crítica de Redentores (1925), su última novela.
+        <br />
+        <br />
+        <span>
+          La Editorial, Universidad de Puerto Rico | March 2010 | ISBN:
+          978-0847704309
+        </span>
+        <br />
+        <br />
+        <span className="bold"> Publication Date:</span>
+        <br />
+        <span>&nbsp;2010</span>
+      </p>
+    ),
   },
   2: {
     title: 'Love and Politics in the Contemporary Spanish American Novel',
