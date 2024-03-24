d135640cd7c27e16ee753f4f0d33cc4fe8e3d93e
Susan Zea <susanzea16@gmail.com>
2024-03-24 17:38:12 -0400

    finished updating book descriptions

--- app/books/bookData.js
+++ app/books/bookData.js
@@ -21,10 +21,7 @@ export const booksByIdx = {
         Poniatowska and Fernando Vallejo to Roberto Bolaño.
         <br />
         <br />
-        <a
-          className="link"
-          href="https://upittpress.org/books/9780822965046/"
-        >
+        <a className="link" href="https://upittpress.org/books/9780822965046/">
           Read more
         </a>
         <br />
@@ -66,7 +63,7 @@ export const booksByIdx = {
         </span>
         <br />
         <br />
-        <span className="bold"> Publication Date:</span>
+        <span className="bold">Publication Date:</span>
         <br />
         <span>&nbsp;2010</span>
       </p>
@@ -75,18 +72,111 @@ export const booksByIdx = {
   2: {
     title: 'Love and Politics in the Contemporary Spanish American Novel',
     image: '/love-and-politics.jpg',
-    description: 'this is a book description 1',
+    description: (
+      <p>
+        The Latin American Literary Boom was marked by complex novels steeped in
+        magical realism and questions of nationalism, often with themes of
+        surreal violence. In recent years, however, those revolutionary projects
+        of the sixties and seventies have given way to quite a different
+        narrative vision and ideology. Dubbed the new sentimentalism, this trend
+        is now keenly elucidated in Love and Politics in the Contemporary
+        Spanish American Novel. Offering a rich account of the rise of this new
+        mode, as well as its political and cultural implications, Aníbal
+        González delivers a close reading of novels by Miguel Barnet, Elena
+        Poniatowska, Isabel Allende, Alfredo Bryce Echenique, Gabriel García
+        Márquez, Antonio Skármeta, Luis Rafael Sánchez, and others. González
+        proposes that new sentimental novels are inspired principally by a
+        desire to heal the division, rancor, and fear produced by decades of
+        social and political upheaval. Valuing pop culture above the
+        avant-garde, such works also tend to celebrate agape—the love of one’s
+        neighbor—while denouncing the negative effects of passion (eros).
+        Illuminating these and other aspects of post-Boom prose, Love and
+        Politics in the Contemporary Spanish American Novel takes a fresh look
+        at contemporary works.
+        <br />
+        <br />
+        <span>
+          University of Texas Press | February 2010 | ISBN: 978-0-292-72131-9
+        </span>
+        <br />
+        <br />
+        <span className="bold">Publication Date:</span>
+        <br />
+        <span>&nbsp;2010</span>
+      </p>
+    ),
   },
   3: {
     title: 'A Companion to Spanish American Modernismo',
     image: '/companion-modernismo.jpg',
-    description: 'this is a book description 0',
+    description: (
+      <p>
+        Modernismo, a literary movement of fundamental importance to Spanish
+        America and Spain, occurred at the turn of the nineteenth century,
+        roughly from the 1880s to the 1920s. It is widely regarded as the first
+        Spanish-language literary movement that originated in the New World and
+        that became influential in the “Mother Country,” Spain. Characterized by
+        the appropriation of French Symbolist aesthetics into Spanish-language
+        literature, modernismo’s other significant traits were its cultural
+        cosmopolitanism, its philological concern with language, literary
+        history, and literary technique, and its journalistic penchant for
+        novelty and fashion. Despite the splendor of modernista poetry,
+        modernismo is now understood as a broad movement whose impact was felt
+        just as strongly in the prose genres: the short story, the novel, the
+        essay, and the journalistic crónica [chronicle]. Conceived as an
+        introduction to modernismo as well as an account of the current state of
+        the art of modernismo studies, this book examines the movement’s
+        contribution to the various Spanish American literary genres, its main
+        authors [from Martí and Nájera to Darío and Rodó], its social and
+        historical context, and its continuing relevance to the work of
+        contemporary Spanish American authors such as Gabriel García Márquez,
+        Sergio Ramírez, and Mario Vargas Llosa.
+        <br />
+        <br />
+        <span>Tamesis Books | August 2007 | ISBN: 9781855661455</span>
+        <br />
+        <br />
+        <span className="bold">Publication Date:</span>
+        <br />
+        <span>&nbsp;2007</span>
+      </p>
+    ),
   },
 
   4: {
     title:
       'Killer Books: Writing, Violence, and Ethics in Modern Spanish American Narrative',
     image: '/killer-books.jpg',
-    description: 'this is a book description 2',
+    description: (
+      <p>
+        Writing and violence have been inextricably linked in Spanish America
+        from the Conquest onward. Spanish authorities used written edicts, laws,
+        permits, regulations, logbooks, and account books to control indigenous
+        peoples whose cultures were predominantly oral, giving rise to a mingled
+        awe and mistrust of the power of the written word that persists in
+        Spanish American culture to the present day. In this masterful study,
+        Aníbal González traces and describes how Spanish American writers have
+        reflected ethically in their works about writing’s relation to violence
+        and about their own relation to writing. Using an approach that owes
+        much to the recent “turn to ethics” in deconstruction and to the works
+        of Jacques Derrida and Emmanuel Levinas, he examines selected short
+        stories and novels by major Spanish American authors from the late
+        nineteenth through the twentieth centuries: Manuel Gutiérrez Nájera,
+        Manuel Zeno Gandía, Teresa de la Parra, Jorge Luis Borges, Alejo
+        Carpentier, Gabriel García Márquez, and Julio Cortázar. He shows how
+        these authors frequently display an attitude he calls “graphophobia,” an
+        intense awareness of the potential dangers of the written word.
+        <br />
+        <br />
+        <span>
+          University of Texas Press | October 2002 | ISBN: 978-0-292-71808-1
+        </span>
+        <br />
+        <br />
+        <span className="bold">Publication Date:</span>
+        <br />
+        <span>&nbsp;2002</span>
+      </p>
+    ),
   },
 };
