951a72054483e0f905fba0aa3c5c7383f49d6e78
Susan Zea <susanzea16@gmail.com>
2024-03-24 15:54:24 -0400

    mobile view books index nav implemented and now going to wire. adjusted styling to accomodate cover container

--- app/books/BookDetails.jsx
+++ app/books/BookDetails.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
+import IconButton from '../shared components/Buttons/Icon/IconButton';
 
 const BookDetails = ({ selectedBook }) => {
   return (
     <div id="book-details">
-      <img
-        className="cover"
-        alt={'photo of book cover'}
-        src={selectedBook.image}
-      />
+      <div className="cover-container">
+        <IconButton icon={'left'} />
+        <img
+          className="cover"
+          alt={'photo of book cover'}
+          src={selectedBook.image}
+        />
+        <IconButton icon={'right'} />
+      </div>
 
       <div className="text">
         <h2>{selectedBook.title}</h2>
